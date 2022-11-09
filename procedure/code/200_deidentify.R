70e38a1beda3bda9bcfe8e71d614d21c83873ec8
sfbardin <sfbardin@asu.edu>
2022-11-09 16:07:26 -0700

    Updated cleaning code

--- procedure/code/200_deidentify.R
+++ procedure/code/200_deidentify.R
@@ -1,11 +1,39 @@
 library(here)
 library(tidyverse)
+library(qualtRics)
+library(svDialogs)
 
 
-raw_rpr_responses <- readRDS(here("data","raw","private","raw_rpr_responses.rds"))
-  head(raw_rpr_responses)
-  str(raw_rpr_responses)
+# Code for downloading survey data from Qualtrics API
+qualtrics_url <- dlgInput("Qualtrics URL:","asu.co1.qualtrics.com")$res
+qualtrics_key <- dlgInput("Qualtrics API Key:", )$res 
+rpr_survey_id <- dlgInput("Survey ID:","SV_e2Id3c5vvtqtazI")$res
+# find API key and survey ID in Qualtrics "my account" info --> Qualtrics IDs menu
+# find URL in the address bar while you're logged into your Qualtrics projects
 
+qualtrics_api_credentials(api_key = qualtrics_key, 
+                          base_url = qualtrics_url, # e.g. "middlebury.az1.qualtrics.com"
+                          install = TRUE,
+                          overwrite = TRUE)
+
+readRenviron("~/.Renviron")
+
+# get survey metadata as nested lists, incl questions and possible responses
+rpr_survey_metadata <- metadata(rpr_survey_id)
+
+# get data frame with four columns with question ID, name, text and required 
+rpr_survey_questions <- survey_questions(rpr_survey_id)
+
+# get survey responses as data frame - ordinal responses are ordered factors
+rpr_responses <- fetch_survey(rpr_survey_id)
+
+# Save permenant raw files and associated documentation for archvial purposes
+saveRDS(rpr_responses, here("data","raw","private","raw_rpr_responses.rds"))
+saveRDS(rpr_survey_questions, here("data","raw","private","raw_rpr_survey_questions.rds"))
+saveRDS(rpr_survey_metadata, here("data","raw","private","raw_rpr_survey_metadata.rds"))
+
+
+# De-identify file to create public raw version
 hegs_rpr_deidentified <- raw_rpr_responses %>% 
                       select(-c(RecipientLastName, 
                                 RecipientFirstName, 
--- procedure/code/201_clean.R
+++ procedure/code/201_clean.R
@@ -1,6 +1,19 @@
-library(here)
-library(tidyverse)
-library(janitor)
+# list of required packages
+packages = c("here","tidyverse")
+
+# load and install required packages
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+# save the R processing environment to r_environment.txt
+writeLines(capture.output(sessionInfo()),here("procedure","environment","r_environment.txt"))
 
 #--------------------------------------------#
 
@@ -16,6 +29,7 @@ int_hegs_rpr <- raw_hegs_rpr %>% filter(Progress > 70 & Q1 != "Under 18")
 #- Backcode other specify responses -#
 
 #------------------------------------#
+  
 table(toupper(int_hegs_rpr$Q3_5_TEXT))
 
 #-Open response areas of specialization to recode-#
@@ -45,9 +59,69 @@ levels(int_hegs_rpr$Q3_recoded) <- c("Human geography",
 table(int_hegs_rpr$Q3_recoded,int_hegs_rpr$Q3)
 ## Note: 2 respondents skipped Q3 and 1 respondent who indicated "other" did not specify what their subfield is
 
-##Unsure how to backcode these variables yet
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+res_scientist <- c("Government scientist (applied)","Project Scientist","Researcher (policy)",
+                   "Senior Research Fellow", "Senior Consultant and external lecturer")
+
+int_hegs_rpr <- int_hegs_rpr %>% 
+  mutate(Q19_recoded = as.factor(ifelse(Q19 %in% res_scientist, 5, 
+                                       ifelse(Q19 == "Lecturer/teacher at a university of applied sciences.", 2, Q19))))
+
+levels(int_hegs_rpr$Q19_recoded) <- c("Full professor/lecturer",
+                                      "Associate professor/lecturer",
+                                      "Assistant professor/lecturer",
+                                      "Laboratory director/head",
+                                      "Research scientist",
+                                      "Post-doctoral fellow",
+                                      "Graduate student (PhD, masters)",
+                                      "Technician/research assistant",
+                                      "Other (specify)",
+                                      "NA")
+
+table(int_hegs_rpr$Q19_recoded,toupper(int_hegs_rpr$Q19))
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+##Unsure how to backcode the Q16 series of variables
+int_hegs_rpr %>% filter(!is.na(Q16_5)) %>% select(c(Q16_1,Q16_2,Q16_3,Q16_4,Q16_5_TEXT)) 
+
+int_hegs_rpr %>% filter(!is.na(Q16_5)) %>% select(c(Q16_5_TEXT)) 
 table(toupper(int_hegs_rpr$Q16_5_TEXT))
-table(int_hegs_rpr$Q19,toupper(int_hegs_rpr$Q19_9_TEXT))
+
+# Should we create an "all co-authors option"; many of these "other" responses also checked Principal investigator of the study
+#      - all co-authors on a publication
+#      - All researchers on the team 
+#      - Students and first authors  
+#      - Researchers (scientist, students, post doc, Principle investigator, etc)  who are producing the data should be ethical enough to produce  reproducible data
+#      - co-investigators 
+
+# Should we create a "no one" option
+#      - Nobody?
+#      - Noone
+#      -  Question assumes that reproducibility should be enforced - dont agree
+
+#Responses that should likely continue to be coded as "other"
+#      - Lab technician
+#      - first-author                [IS THIS DISTINCT ENOUGH FROM PRINCIPAL INVESTIGATOR OF THE STUDY?]
+#      - Supervisors of PhDs         [IS THIS DISTINCT ENOUGH FROM PRINCIPAL INVESTIGATOR OF THE STUDY?]
+#      - Researcher's institution should provide more incentives to consider reproducibility
+#      - every researchers should have this goal in mind as an ethical requirement, and it should be a requirement  at least 
+#           where there is public funding (which is the case in a lot of priva
+#      - Whoever wants to test a given case.
+
+don't know
+see earlier comment.
+
+
+
+
+
+Everyone       [NEED TO RECODE ALL Q16 SERIES]      
+All of the above, especially the PI, but also researchers who attempt to reproduce.       [NEED TO RECODE ALL Q16 SERIES] 
+Researchers (scientist, students, post doc, Principle investigator, etc)  who are producing the data should be ethical enough to produce  reproducible data [NEED TO RECODE Q16_1]
 
 
 #----------------------------------------------------------#
